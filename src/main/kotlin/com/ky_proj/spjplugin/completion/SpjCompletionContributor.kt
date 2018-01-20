b22add3b9fc40d2dd03b0c3caa2eb0f88a44c64c
Y K <orenoikizama@hotmail.com>
2018-01-20 16:30:32 +0900

    argumentのcompletionとそのテスト

--- src/main/kotlin/com/ky_proj/spjplugin/completion/SpjCompletionContributor.kt
+++ src/main/kotlin/com/ky_proj/spjplugin/completion/SpjCompletionContributor.kt
@@ -3,50 +3,62 @@ package com.ky_proj.spjplugin.completion
 /**
  * Created on 2018/01/19.
  */
-import com.intellij.codeInsight.AutoPopupController
 import com.intellij.codeInsight.completion.*
-import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.codeInsight.lookup.LookupElementPresentation
-import com.intellij.ide.highlighter.JavaFileType
-import com.intellij.lang.ASTNode
-import com.intellij.lang.PsiBuilder
-import com.intellij.lang.PsiBuilderUtil
-import com.intellij.mock.MockFileManager
-import com.intellij.mock.MockPsiManager
-import com.intellij.openapi.components.BaseComponent
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorModificationUtil
-import com.intellij.openapi.editor.impl.EditorImpl
-import com.intellij.openapi.extensions.ExtensionPointName
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Condition
-import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.objectTree.ObjectTree
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.psi.*
-import com.intellij.psi.impl.PsiElementFactoryImpl
 import com.intellij.psi.tree.IElementType
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
-import com.intellij.util.messages.MessageBus
 import com.ky_proj.spjplugin.completion.inserthandler.CommandInsertHandler
-import com.ky_proj.spjplugin.filetype.SpjFileType
 import com.ky_proj.spjplugin.icon.SpjIcon
 import com.ky_proj.spjplugin.language.SpjLanguage
 import com.ky_proj.spjplugin.psi.*
-import com.ky_proj.spjplugin.psi.impl.SpjNamedElementImpl
-import com.ky_proj.spjplugin.psi.impl.SpjProcedureDefImpl
 import com.ky_proj.spjplugin.util.*
-import com.ky_proj.spjplugin.util.SpjUtil
-import org.picocontainer.PicoContainer
-
-import java.util.ArrayList
-import java.util.Collections
 
 class SpjCompletionContributor : CompletionContributor() {
+
+    /*
+        指定されたタイプのcompletionにNeoの組み込み関数と、関数形式で呼び出せる定義済みプロシージャを加える
+     */
+    private fun addFunctions(elementtype :IElementType){
+        extend(CompletionType.BASIC,
+                PlatformPatterns.psiElement(elementtype).withLanguage(SpjLanguage.INSTANCE),
+                object : CompletionProvider<CompletionParameters>() {
+                    public override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet) {
+                        // 組み込み関数
+                        val list = SpjFunctionProvider.list(parameters.originalFile.project)
+                        list.forEach { command ->
+                            val element = LookupElementBuilder.create(command, command.node.text)
+                                    .withTypeText("built in function", true)
+                                    .withInsertHandler(CommandInsertHandler())
+                                    .withIcon(SpjIcon.FILE)
+                            resultSet.addElement(element)
+                        }
+//                        // 定義済みプロシージャ
+//                        if (SpjUtil.GET_SPJ_SETTING.isEnhanceMode(parameters.originalFile.project)) {
+//                            addProceduresToResultSet(resultSet, true)
+//                        }
+                    }
+                }
+        )
+    }
+
+    private fun addVariablesAndArguments(type :IElementType){
+        extend(CompletionType.BASIC,
+                PlatformPatterns.psiElement(type).withLanguage(SpjLanguage.INSTANCE),
+                object : CompletionProvider<CompletionParameters>() {
+                    public override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet) {
+                        // 変数と引数
+                        val vars = GetVariables(parameters.originalFile)
+                        val variables = vars.variables
+                        for (variable in variables) {
+                            resultSet.addElement(LookupElementBuilder.create(variable))
+                            resultSet.addElement(LookupElementBuilder.create("$" + variable))
+                        }
+                    }
+                }
+        )
+
+    }
 
     init {
         // コマンド
@@ -68,92 +80,19 @@ class SpjCompletionContributor : CompletionContributor() {
 
 
         // 変数
-        extend(CompletionType.BASIC,
-                PlatformPatterns.psiElement(SpjTypes.VARIABLE).withLanguage(SpjLanguage.INSTANCE),
-                object : CompletionProvider<CompletionParameters>() {
-                    public override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet) {
-                        // 組み込み関数
-                        val list = SpjFunctionProvider.list(parameters.originalFile.project)
-                        list.forEach { command ->
-                            val element = LookupElementBuilder.create(command, command.node.text)
-                                    .withTypeText("built in function", true)
-                                    .withInsertHandler(CommandInsertHandler())
-                                    .withIcon(SpjIcon.FILE)
-                            resultSet.addElement(element)
-                        }
-//                        // 定義済みプロシージャ
-//                        if (SpjUtil.GET_SPJ_SETTING.isEnhanceMode(parameters.originalFile.project)) {
-//                            addProceduresToResultSet(resultSet, true)
-//                        }
+        addFunctions(SpjTypes.VARIABLE)
+        addVariablesAndArguments(SpjTypes.VARIABLE)
 
-                        // 変数と引数
-                        val vars = GetVariables(parameters.originalFile)
-                        val variables = vars.variables
-                        for (variable in variables) {
-                            resultSet.addElement(LookupElementBuilder.create(variable))
-                            resultSet.addElement(LookupElementBuilder.create("$" + variable))
-                        }
-                    }
-                }
-        )
-
-        /*
         // 関数
-        extend(CompletionType.BASIC,
-                PlatformPatterns.psiElement(SpjTypes.FUNCTION).withLanguage(SpjLanguage.INSTANCE),
-                object : CompletionProvider<CompletionParameters>() {
-                    public override fun addCompletions(parameters: CompletionParameters,
-                                                       context: ProcessingContext,
-                                                       resultSet: CompletionResultSet) {
+        addFunctions(SpjTypes.FUNCTION)
+        addVariablesAndArguments(SpjTypes.FUNCTION)
 
-                        // neoの組み込み関数
-                        val commands = SpjUtil.GET_FUNCTIONS.getList()
-                        for (i in commands.indices) {
-                            resultSet.addElement(LookupElementBuilder.create(commands[i]))
-                        }
-
-                        // 定義済みプロシージャ
-                        if (SpjUtil.GET_SPJ_SETTING.isEnhanceMode(myProject)) {
-                            addProceduresToResultSet(resultSet, true)
-                        }
-
-                    }
-                }
-        )
 
         // 引数
-        extend(CompletionType.BASIC,
-                PlatformPatterns.psiElement(SpjTypes.ARGUMENT).withLanguage(SpjLanguage.INSTANCE),
-                object : CompletionProvider<CompletionParameters>() {
-                    public override fun addCompletions(parameters: CompletionParameters,
-                                                       context: ProcessingContext,
-                                                       resultSet: CompletionResultSet) {
+        addFunctions(SpjTypes.ARGUMENT)
+        addVariablesAndArguments(SpjTypes.ARGUMENT)
 
-                        // 変数と引数
-                        val vars = GetVariables(myFile)
-                        val variables = vars.getVariables()
-                        for (`var` in variables) {
-                            resultSet.addElement(LookupElementBuilder.create(`var`))
-                            resultSet.addElement(LookupElementBuilder.create("$" + `var`))
-                        }
-
-
-                        // 関数
-                        val commands = SpjUtil.GET_FUNCTIONS.getList()
-                        for (i in commands.indices) {
-                            resultSet.addElement(LookupElementBuilder.create(commands[i]))
-                        }
-
-                        // 定義済みプロシージャ
-                        if (SpjUtil.GET_SPJ_SETTING.isEnhanceMode(myProject)) {
-                            addProceduresToResultSet(resultSet, true)
-                        }
-
-
-                    }
-                }
-        )
-
+        /*
         // プロシージャ呼び出し
         extend(CompletionType.BASIC,
                 PlatformPatterns.psiElement(SpjTypes.PROCEDURE_CALL).withLanguage(SpjLanguage.INSTANCE),
