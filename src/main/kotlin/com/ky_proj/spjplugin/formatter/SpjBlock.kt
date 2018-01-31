6152b58018cd913c48fd65cf174f21847f4d089b
Y K <orenoikizama@hotmail.com>
2018-01-31 09:39:10 +0900

    code style setting

---
+++ src/main/kotlin/com/ky_proj/spjplugin/formatter/SpjBlock.kt
@@ -0,0 +1,83 @@
+package com.ky_proj.spjplugin.formatter
+
+import com.intellij.formatting.*
+import com.intellij.lang.ASTNode
+import com.intellij.psi.TokenType
+import com.intellij.psi.formatter.common.AbstractBlock
+import com.intellij.psi.tree.TokenSet
+import com.ky_proj.spjplugin.psi.SpjTypes
+
+import com.ky_proj.spjplugin.psi.SpjTypes.*
+
+import java.util.ArrayList
+
+class SpjBlock constructor(node: ASTNode, wrap: Wrap?, alignment: Alignment?, private val spacingBuilder: SpacingBuilder) : AbstractBlock(node, wrap, alignment) {
+    private val myIndent: Indent
+
+    private val firstChildAlignment: Alignment?
+        get() {
+            val subBlocks = subBlocks
+            for (subBlock in subBlocks) {
+                val alignment = subBlock.alignment
+                if (alignment != null) {
+                    return null
+                }
+            }
+            return null
+        }
+
+    init {
+
+        myIndent = SpjIndentProcessor().getChildIndent(node)
+    }
+
+    override fun buildChildren(): List<Block> {
+        val blocks = ArrayList<Block>()
+        if (isLeaf) {
+            return blocks
+        }
+        if (myNode.elementType === TokenType.ERROR_ELEMENT || myNode.elementType === TokenType.WHITE_SPACE) {
+            return blocks
+        }
+        if (myNode.textRange.length == 0) {
+            return blocks
+        }
+        var child: ASTNode? = myNode.firstChildNode
+        var previousChild: ASTNode? = null
+        while (child != null) {
+            if (child.elementType !== TokenType.WHITE_SPACE && child.elementType !== TokenType.ERROR_ELEMENT && child.textRange.length != 0 &&
+                    (previousChild == null || previousChild.elementType !== SpjTypes.CRLF || child.elementType !== SpjTypes.CRLF)) {
+                val block = SpjBlock(child, Wrap.createWrap(WrapType.NORMAL, false), Alignment.createAlignment(), spacingBuilder)
+                blocks.add(block)
+            }
+            previousChild = child
+            child = child.treeNext
+        }
+        return blocks
+    }
+
+    override fun getIndent(): Indent? {
+        val parent = myNode.treeParent
+        return myIndent
+    }
+
+    override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
+        return ChildAttributes(childIndent, firstChildAlignment)
+    }
+
+    override fun getChildIndent(): Indent? {
+
+        return if (TokenSet.create(IFBLOCK, FORBLOCK, WHILEBLOCK, TRYBLOCK).contains(myNode.elementType)) {
+            Indent.getNormalIndent()
+        } else Indent.getNoneIndent()
+
+    }
+
+    override fun getSpacing(child1: Block?, child2: Block): Spacing? {
+        return spacingBuilder.getSpacing(this, child1, child2)
+    }
+
+    override fun isLeaf(): Boolean {
+        return myNode.firstChildNode == null
+    }
+}
---
+++ src/main/kotlin/com/ky_proj/spjplugin/formatter/SpjFormattingModelBuilder.kt
@@ -0,0 +1,43 @@
+package com.ky_proj.spjplugin.formatter
+
+import com.intellij.formatting.*
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings
+import com.ky_proj.spjplugin.psi.SpjTypes
+import com.ky_proj.spjplugin.language.SpjLanguage
+
+import com.ky_proj.spjplugin.psi.SpjTypes.*
+
+class SpjFormattingModelBuilder : FormattingModelBuilder {
+    override fun createModel(element: PsiElement, settings: CodeStyleSettings): FormattingModel {
+        val commonSettings = settings.getCommonSettings(SpjLanguage.INSTANCE)
+        val spacingBuilder = createSpacingBuilder(commonSettings)
+
+        val block = SpjBlock(element.node, Wrap.createWrap(WrapType.NORMAL, false), Alignment.createAlignment(), spacingBuilder)
+
+        return FormattingModelProvider.createFormattingModelForPsiFile(element.containingFile, block, settings)
+    }
+
+    private fun createSpacingBuilder(settings: CommonCodeStyleSettings): SpacingBuilder {
+
+        val codeStyleSettings = CodeStyleSettings()
+
+        return SpacingBuilder(codeStyleSettings, SpjLanguage.INSTANCE)
+                .before(COMMA).spaceIf(settings.SPACE_BEFORE_COMMA)
+                .after(COMMA).spaceIf(settings.SPACE_AFTER_COMMA)
+                .before(SpjTypes.SEPARATOR).spaceIf(settings.SPACE_AROUND_ASSIGNMENT_OPERATORS)
+                .after(SpjTypes.SEPARATOR).spaceIf(settings.SPACE_AROUND_ASSIGNMENT_OPERATORS)
+                .before(SpjTypes.ORAND).spaceIf(settings.SPACE_AROUND_LOGICAL_OPERATORS)
+                .after(SpjTypes.ORAND).spaceIf(settings.SPACE_AROUND_LOGICAL_OPERATORS)
+                .before(SpjTypes.OPER).spaceIf(settings.SPACE_AROUND_LOGICAL_OPERATORS)
+                .after(SpjTypes.OPER).spaceIf(settings.SPACE_AROUND_LOGICAL_OPERATORS)
+    }
+
+    override fun getRangeAffectingIndent(psiFile: PsiFile, i: Int, astNode: ASTNode): TextRange? {
+        return null
+    }
+}
