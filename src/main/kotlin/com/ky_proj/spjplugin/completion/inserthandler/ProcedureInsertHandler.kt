49b449336f9d3f2aeeb56cfcec1a1f87c2502225
Y K <orenoikizama@hotmail.com>
2018-02-06 09:21:33 +0900

    InsertHandlerの改良

--- src/main/kotlin/com/ky_proj/spjplugin/completion/inserthandler/ProcedureInsertHandler.kt
+++ src/main/kotlin/com/ky_proj/spjplugin/completion/inserthandler/ProcedureInsertHandler.kt
@@ -4,22 +4,14 @@ import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.TokenSet
 import com.ky_proj.spjplugin.psi.SpjTypes
 
 /**
  * Created on 2018/01/20.
  */
-class ProcedureInsertHandler : InsertHandler<LookupElement> {
-
+class ProcedureInsertHandler :SpjInsertHandlerBase(){
     override fun handleInsert(context: InsertionContext, item: LookupElement) {
-        val editor = context.editor
-        val procedureDef = item.`object` as PsiElement
-        val procedureName = procedureDef.node.findChildByType(SpjTypes.PROCEDURE)?.text ?:return
-        //EditorModificationUtil.insertStringAtCaret(editor, "insert")
-        // 引数の定義があるならそこにcaretを移動する
-        if(procedureDef.node.findChildByType(SpjTypes.ARGUMENTS) != null){
-            editor.caretModel.moveToOffset(context.startOffset + procedureName.length + "(".length)
-        }
-
+        super.handleInsert(context, item)
     }
 }
---
+++ src/main/kotlin/com/ky_proj/spjplugin/completion/inserthandler/SpjInsertHandlerBase.kt
@@ -0,0 +1,33 @@
+package com.ky_proj.spjplugin.completion.inserthandler
+
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.TokenSet
+import com.ky_proj.spjplugin.psi.SpjTypes
+
+open class SpjInsertHandlerBase : InsertHandler<LookupElement> {
+
+    override fun handleInsert(context: InsertionContext, item: LookupElement) {
+        execHandleInsert(context, item)
+    }
+
+    private fun execHandleInsert(context: InsertionContext, item: LookupElement) {
+        val editor = context.editor
+        val node = (item.`object` as PsiElement).node
+        val commandName = node.findChildByType(TokenSet.create(SpjTypes.COM_CALL, SpjTypes.FUNCTION, SpjTypes.PROCEDURE))?.text ?: return
+
+        // 引数の定義があるならそこにcaretを移動する
+
+        val arguments = node.findChildByType(SpjTypes.ARGUMENTS) ?:return
+        val startOffset = context.startOffset + commandName.length + "(".length
+        editor.caretModel.moveToOffset(startOffset)
+
+        // 引数に中身があるならそこにcaretを移動してさらに最初の引数を選択状態にする
+        val firstArgument = arguments.findChildByType(TokenSet.create(SpjTypes.ARGS)) ?: return
+        val endOffset = startOffset + firstArgument.text.length
+        editor.selectionModel.setSelection(startOffset, endOffset)
+        editor.caretModel.moveToOffset(endOffset)
+    }
+}
